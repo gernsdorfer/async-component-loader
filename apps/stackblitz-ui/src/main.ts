bf657b6a4254d5d6aec6ebb8d9ed71ddaef02784
gernsdorfer <larswiedemann@gmx.net>
2022-07-12 14:36:55 +0200

    fix stackblitz app

--- apps/stackblitz-ui/src/main.ts
+++ apps/stackblitz-ui/src/main.ts
@@ -1,17 +1,10 @@
-import {enableProdMode, importProvidersFrom} from '@angular/core';
-import {environment} from './environments/environment';
+import {importProvidersFrom} from '@angular/core';
 import {bootstrapApplication, BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule),
-
-
   ]
 })
 
